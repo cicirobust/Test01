613cba911a1c50cc3bf9b25c5f3b94c0bea42af7
cicirobust <162078553+cicirobust@users.noreply.github.com>
2024-03-06 20:21:32 +1030

    Create question16.sh

---
+++ question16.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ian-knight-uofa/git-practice-02
+cd git-practice-02
+git checkout branch2
+git merge branch3
+git add file1
+git commit -m "conflicts resolved"
+git branch -d branch3
